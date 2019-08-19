02f5be0eedd93419671e997554e4b0a3eda7bbdf
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-08-19 22:41:36 +0300

    ADD Event Data Structure

--- src/data.js
+++ src/data.js
@@ -51,3 +51,91 @@ const tripFiltersConfig = {
   titles: new Set([`Everything`, `Future`, `Past`])
 };
 
+const getRandomDestination = (type) => {
+  if (type === `Flight`) {
+    return utils.getRandomElement(eventConfig.destination.citys);
+  }
+
+  if (type === `Check-in`) {
+    return utils.getRandomElement(eventConfig.destination.checkinPoints);
+  }
+
+  if (type === `Sightseeing`) {
+    return utils.getRandomElement(eventConfig.destination.sights);
+  }
+
+  if (type === `Restaurant`) {
+    return utils.getRandomElement(eventConfig.destination.eatingPoints);
+  }
+
+  if (eventConfig.types.transport.has(type)) {
+    return utils.getRandomElement(new Set([...eventConfig.destination.citys,
+      ...eventConfig.destination.sights,
+      ...eventConfig.destination.eatingPoints,
+      ...eventConfig.destination.checkinPoints]));
+  }
+
+  return `Error`;
+};
+
+const getRandomEventTime = (past, future) => {
+  let start = Date.now()
+  + utils.getRandomNumber(past, future - 1) * utils.MILLISECONDS_IN_DAY
+  + utils.getRandomNumber(0, utils.HOURS_IN_DAY - 1) * utils.MILLISECONDS_IN_HOUR
+  + utils.getRandomNumber(0, utils.MINUTES_IN_HOUR - 1) * utils.MILLISECONDS_IN_MINUTE;
+
+  let end = start
+  + utils.getRandomNumber(0, utils.HOURS_IN_DAY - 1) * utils.MILLISECONDS_IN_HOUR
+  + utils.getRandomNumber(0, utils.MINUTES_IN_HOUR - 1) * utils.MILLISECONDS_IN_MINUTE;
+
+  return {
+    start,
+    end
+  };
+};
+
+const getOffer = (offerDescription) => ({
+  description: offerDescription,
+  price: utils.getRandomNumber(offerConfig.price.min, offerConfig.price.max),
+  isCheck: utils.getRandomFlag()
+});
+
+const getEventData = () => {
+  const randomType = utils.getRandomElement(new Set([...eventConfig.types.transport, ...eventConfig.types.arrival]));
+
+  return {
+    type: randomType,
+    destination: getRandomDestination(randomType),
+    description: utils.shuffle(eventConfig.descriptions.sentences)
+      .slice(0, utils.getRandomNumber(eventConfig.descriptions.minAmount, eventConfig.descriptions.maxAmount))
+      .join(`. `),
+    time: getRandomEventTime(eventConfig.periodOfTime.past, eventConfig.periodOfTime.future),
+    price: utils.getRandomNumber(eventConfig.price.min, eventConfig.price.max),
+    offers: utils.shuffle(offerConfig.descriptions)
+      .slice(0, utils.getRandomNumber(offerConfig.minAmount, offerConfig.maxAmount))
+      .map((offerDescription) => getOffer(offerDescription)),
+    photos: new Array(utils.getRandomNumber(eventConfig.photos.minAmount, eventConfig.photos.maxAmount))
+      .fill(``)
+      .map(() => eventConfig.photos.defaultURL + Math.random()),
+    isFavorite: utils.getRandomFlag(),
+
+    get isTransportType() {
+      return eventConfig.types.transport.has(this.type);
+    },
+    get timeDuration() {
+      return {
+        duration: this.time.end - this.time.start,
+
+        get days() {
+          return Math.floor(this.duration / utils.MILLISECONDS_IN_DAY);
+        },
+        get hours() {
+          return Math.floor((this.duration - this.days * utils.MILLISECONDS_IN_DAY) / utils.MILLISECONDS_IN_HOUR);
+        },
+        get minutes() {
+          return Math.floor((this.duration - this.days * utils.MILLISECONDS_IN_DAY - this.hours * utils.MILLISECONDS_IN_HOUR) / utils.MILLISECONDS_IN_MINUTE);
+        }
+      };
+    }
+  };
+};
