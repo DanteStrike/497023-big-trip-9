cb950b238608ab9403cd52083b2b8f5974e9adfa
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-08-19 22:49:03 +0300

    ADD dynamics in trip filters component

--- src/components/trip-filters.js
+++ src/components/trip-filters.js
@@ -1,18 +1,18 @@
-export const createTripFiltersTemplate = () =>
+export const createTripFiltersTemplate = (filters) =>
   `<form class="trip-filters" action="#" method="get">
     <div class="trip-filters__filter">
       <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked="">
-      <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      <label class="trip-filters__filter-label" for="filter-everything">${filters[0].title}</label>
     </div>
 
     <div class="trip-filters__filter">
       <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-      <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      <label class="trip-filters__filter-label" for="filter-future">${filters[1].title}</label>
     </div>
 
     <div class="trip-filters__filter">
       <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-      <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      <label class="trip-filters__filter-label" for="filter-past">${filters[2].title}</label>
     </div>
 
     <button class="visually-hidden" type="submit">Accept filter</button>
