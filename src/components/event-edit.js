31c20e0c0ab9d7f187517e57e0d05ebc3a903d36
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-08-20 10:00:59 +0300

    FIX offers buttons

--- src/components/event-edit.js
+++ src/components/event-edit.js
@@ -124,9 +124,9 @@ export const createEditEventTemplate = ({type, isTransportType, description, des
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
           <div class="event__available-offers">
-            ${offers.map((offer) => `<div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${offer.isCheck ? `checked` : ``}>
-              <label class="event__offer-label" for="event-offer-luggage-1">
+            ${offers.map((offer, index) => `<div class="event__offer-selector">
+            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-${index}" type="checkbox" name="event-offer-luggage" ${offer.isCheck ? `checked` : ``}>
+              <label class="event__offer-label" for="event-offer-luggage-${index}">
                 <span class="event__offer-title">${offer.description}</span>
                 +
                 €&nbsp;<span class="event__offer-price">${offer.price}</span>
