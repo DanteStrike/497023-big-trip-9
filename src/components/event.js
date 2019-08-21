4a32c358cae3426f09cd1b4ef18fed56993359aa
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-08-21 19:28:53 +0300

    ADD components classes

--- src/components/event.js
+++ src/components/event.js
@@ -1,40 +1,90 @@
-export const createEventTemplate = ({type, isTransportType, destination, time, timeDuration, price, offers}) =>
-  `<li class="trip-events__item">
-    <div class="event">
-      <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
-      </div>
-      <h3 class="event__title">${type} ${isTransportType ? `to` : `into`} ${destination}</h3>
+import {createElement, Enum} from '../utils/utils.js';
+import {eventsData} from '../eventsList.js';
 
-      <div class="event__schedule">
-        <p class="event__time">
-          <time class="event__start-time"
-            datetime="${new Date(time.start).getFullYear()}-${new Date(time.start).getMonth()}-${new Date(time.start).getDate()}T${new Date(time.start).getHours()}:${new Date(time.start).getMinutes()}"
-            >${new Date(time.start).getHours()}:${new Date(time.start).getMinutes()}</time>
-          —
-          <time class="event__end-time" datetime="${new Date(time.end).getFullYear()}-${new Date(time.end).getMonth()}-${new Date(time.end).getDate()}T${new Date(time.end).getHours()}:${new Date(time.end).getMinutes()}">
-          ${new Date(time.end).getHours()}:${new Date(time.end).getMinutes()}</time>
+
+class Event {
+  constructor({type, destination, time, price, offers}) {
+    this._type = type;
+    this._destination = destination;
+    this._time = time;
+    this._offers = offers;
+    this._price = price;
+    this._element = null;
+  }
+
+  get isTransportType() {
+    return eventsData.types.transport.has(this._type);
+  }
+
+  get timeDuration() {
+    return {
+      duration: this._time.end - this._time.start,
+
+      get days() {
+        return Math.floor(this.duration / Enum.MILLISECONDS_IN_DAY);
+      },
+      get hours() {
+        return Math.floor((this.duration - this.days * Enum.MILLISECONDS_IN_DAY) / Enum.MILLISECONDS_IN_HOUR);
+      },
+      get minutes() {
+        return Math.floor((this.duration - this.days * Enum.MILLISECONDS_IN_DAY - this.hours * Enum.MILLISECONDS_IN_HOUR) / Enum.MILLISECONDS_IN_MINUTE);
+      }
+    };
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${this._type.toLowerCase()}.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${this._type} ${this.isTransportType ? `to` : `into`} ${this._destination}</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time"
+              datetime="${new Date(this._time.start).getFullYear()}-${new Date(this._time.start).getMonth()}-${new Date(this._time.start).getDate()}T${new Date(this._time.start).getHours()}:${new Date(this._time.start).getMinutes()}"
+              >${new Date(this._time.start).getHours()}:${new Date(this._time.start).getMinutes()}</time>
+            —
+            <time class="event__end-time" datetime="${new Date(this._time.end).getFullYear()}-${new Date(this._time.end).getMonth()}-${new Date(this._time.end).getDate()}T${new Date(this._time.end).getHours()}:${new Date(this._time.end).getMinutes()}">
+            ${new Date(this._time.end).getHours()}:${new Date(this._time.end).getMinutes()}</time>
+          </p>
+          <p class="event__duration">
+            ${this.timeDuration.days !== 0 ? `${this.timeDuration.days}D` : ``} ${this.timeDuration.hours !== 0 ? `${this.timeDuration.hours}H` : ``} ${this.timeDuration.minutes !== 0 ? `${this.timeDuration.minutes}M` : ``}</p>
+        </div>
+
+        <p class="event__price">
+          €&nbsp;<span class="event__price-value">${this._price}</span>
         </p>
-        <p class="event__duration">
-          ${timeDuration.days !== 0 ? `${timeDuration.days}D` : ``} ${timeDuration.hours !== 0 ? `${timeDuration.hours}H` : ``} ${timeDuration.minutes !== 0 ? `${timeDuration.minutes}M` : ``}</p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          ${this._offers.map((offer) => `<li class="event__offer" ${offer.isCheck ? `` : `style="color: gray;"`}>
+          <span class="event__offer-title" ${offer.isCheck ? `` : `style="color: gray;"`}>${offer.description}</span>
+          +
+          €&nbsp;<span class="event__offer-price" ${offer.isCheck ? `` : `style="color: gray;"`}>${offer.price}</span>
+          </li>`).join(``)}
+
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
       </div>
+    </li>`.trim();
+  }
+}
 
-      <p class="event__price">
-        €&nbsp;<span class="event__price-value">${price}</span>
-      </p>
-
-      <h4 class="visually-hidden">Offers:</h4>
-      <ul class="event__selected-offers">
-        ${offers.map((offer) => `<li class="event__offer" ${offer.isActive ? `` : `style="color: gray;"`}>
-        <span class="event__offer-title" ${offer.isActive ? `` : `style="color: gray;"`}>${offer.description}</span>
-        +
-        €&nbsp;<span class="event__offer-price" ${offer.isActive ? `` : `style="color: gray;"`}>${offer.price}</span>
-        </li>`).join(``)}
-
-      </ul>
-
-      <button class="event__rollup-btn" type="button">
-        <span class="visually-hidden">Open event</span>
-      </button>
-    </div>
-  </li>`.trim();
+export default Event;
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,33 @@
-export const createMenuTemplate = (menuItems) =>
-  `<nav class="trip-controls__trip-tabs  trip-tabs">
-    <a class="trip-tabs__btn ${menuItems[0].isActive ? `trip-tabs__btn--active` : ``}" href="#">${menuItems[0].title}</a>
-    <a class="trip-tabs__btn ${menuItems[1].isActive ? `trip-tabs__btn--active` : ``}" href="#">${menuItems[1].title}</a>
-  </nav>`;
+import {createElement} from '../utils/utils.js';
+
+class Menu {
+  constructor(menuItems) {
+    this._menuItems = menuItems;
+    this._element = null;
+  }
+
+  get citiesAmount() {
+    return new Set(this._cities).size;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn ${this._menuItems[0].isActive ? `trip-tabs__btn--active` : ``}" href="#">${this._menuItems[0].title}</a>
+      <a class="trip-tabs__btn ${this._menuItems[1].isActive ? `trip-tabs__btn--active` : ``}" href="#">${this._menuItems[1].title}</a>
+    </nav>`;
+  }
+}
+
+export default Menu;
