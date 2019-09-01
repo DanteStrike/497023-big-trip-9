4d12e8374623adc9842ec7a64c5be97f416c099b
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-09-01 17:14:41 +0300

    Update configs.js

--- src/configs.js
+++ src/configs.js
@@ -7,26 +7,33 @@ export const eventConfig = {
     past: -1,
     future: 2
   },
-  photos: {
-    minAmount: 2,
-    maxAmount: 5
-  },
-  price: {
-    min: 0,
-    max: 1000
-  },
-
-  offer: {
-    price: {
-      min: 0,
-      max: 600
-    },
-    minAmount: 0,
-    maxAmount: 3
-  }
 };
 
 export const eventsListConfig = {
   minAmount: 20,
   maxAmount: 30
+};
+
+export const destinationConfig = {
+  sentences: {
+    minAmount: 1,
+    maxAmount: 3
+  },
+  photos: {
+    minAmount: 2,
+    maxAmount: 5
+  },
+  price: {
+    min: 400,
+    max: 1000
+  },
+
+  offer: {
+    price: {
+      min: 20,
+      max: 100
+    },
+    minAmount: 0,
+    maxAmount: 3
+  }
 };
