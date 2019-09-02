49a462b5bb7f2c334b7ff41b6af857e11bc0e6ac
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-09-02 12:33:47 +0300

    REWORK data model

--- src/data/events-data.js
+++ src/data/events-data.js
@@ -1,64 +1,78 @@
-import {destinationsData} from './destination-data.js';
-import {eventConfig} from '../configs.js';
-import {getRandomFlag, getRandomElement, shallowCopyObjsArray} from '../utils/utils.js';
+import {getRandomFlag, getRandomElement, getRandomNumber, shuffle} from '../utils/utils.js';
 import getRandomEventTime from '../utils/getRandomEventTime.js';
+import {eventConfig} from '../configs.js';
 
-//  Согласно ТЗ существует две группы типов точек (transfer и activity)
-const eventTypes = {
-  transfer: new Set([`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`]),
-  activity: new Set([`check-in`, `sightseeing`, `restaurant`])
-};
 
-//  Разбить точки назначения по смыслу
-const destinationTypes = {
-  cities: new Set([`Amsterdam`, `Geneva`, `Chamonix`, `France`]),
-  sights: new Set([`Museum`, `Fountain`, `Palace`, `Park`]),
-  eatingPoints: new Set([`Cafe`, `Hotel`, `Motel`]),
-  checkinPoints: new Set([`Hotel`, `Motel`])
+const eventsData = {
+  types: {
+    transfer: new Set([`Taxi`, `Bus`, `Train`, `Ship`, `transfer`, `Drive`, `Flight`]),
+    activity: new Set([`Check-in`, `Sightseeing`, `Restaurant`])
+  },
+  destination: {
+    cities: new Set([`Amsterdam`, `Geneva`, `Chamonix`, `France`]),
+    sights: new Set([`Museum`, `Fountain`, `Palace`, `Park`]),
+    eatingPoints: new Set([`Cafe`, `Hotel`, `Motel`]),
+    checkinPoints: new Set([`Hotel`, `Motel`])
+  },
+  sentences: [`Lorem ipsum dolor sit amet, consectetur adipiscing elit`, `Cras aliquet varius magna, non porta ligula feugiat eget`,
+    `Fusce tristique felis at fermentum pharetra`, `Aliquam id orci ut lectus varius viverra`,
+    `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante`, `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum`,
+    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui`, `Sed sed nisi sed augue convallis suscipit in sed felis`,
+    `Aliquam erat volutpat`, `Nunc fermentum tortor ac porta dapibus`, `In rutrum ac purus sit amet tempus`],
+
+  offerDescriptions: [`Add luggage`, `Switch to comfort class`, `Add meal`, `Choose seats`],
+
+  photosDefaultURL: `http://picsum.photos/300/150?r=`
 };
 
 // Подобрать приблизительно правдоподобную точку назначения согласно типу события.
-const getRandomDestination = (eventType) => {
-  switch (eventType) {
-    case `flight`:
-      return getRandomElement(destinationTypes.cities);
+const getRandomDestination = (type, data) => {
+  switch (type) {
+    case `Flight`:
+      return getRandomElement(data.destination.cities);
 
-    case `check-in`:
-      return getRandomElement(destinationTypes.checkinPoints);
+    case `Check-in`:
+      return getRandomElement(data.destination.checkinPoints);
 
-    case `sightseeing`:
-      return getRandomElement(destinationTypes.sights);
+    case `Sightseeing`:
+      return getRandomElement(data.destination.sights);
 
-    case `restaurant`:
-      return getRandomElement(destinationTypes.eatingPoints);
+    case `Restaurant`:
+      return getRandomElement(data.destination.eatingPoints);
 
     default:
-      return getRandomElement(new Set([...destinationTypes.cities,
-        ...destinationTypes.sights,
-        ...destinationTypes.eatingPoints,
-        ...destinationTypes.checkinPoints]));
+      return getRandomElement(new Set([...data.destination.cities,
+        ...data.destination.sights,
+        ...data.destination.eatingPoints,
+        ...data.destination.checkinPoints]));
   }
 };
 
-//  Сгенерировать мок для 1-ой точки
-const getEventData = () => {
-  const randomType = getRandomElement(new Set([...eventTypes.transfer, ...eventTypes.activity]));
-  const randomDestination = getRandomDestination(randomType, destinationTypes);
+const getOffer = (offerDescription, config) => ({
+  description: offerDescription,
+  price: getRandomNumber(config.offer.price.min, config.offer.price.max),
+  isActive: getRandomFlag()
+});
+const getEventData = (data, config) => {
+  const randomType = getRandomElement(new Set([...data.types.transfer, ...data.types.activity]));
 
   return {
     type: randomType,
-    destination: randomDestination,
-    time: getRandomEventTime(eventConfig.periodOfTime.past, eventConfig.periodOfTime.future),
+    destination: getRandomDestination(randomType, data),
+    description: shuffle(data.sentences)
+      .slice(0, getRandomNumber(config.sentences.minAmount, config.sentences.maxAmount))
+      .join(`. `),
+    time: getRandomEventTime(config.periodOfTime.past, config.periodOfTime.future),
+    price: getRandomNumber(config.price.min, config.price.max),
+    offers: shuffle(data.offerDescriptions)
+      .slice(0, getRandomNumber(config.offer.minAmount, config.offer.maxAmount))
+      .map((offerDescription) => getOffer(offerDescription, config)),
+    photos: new Array(getRandomNumber(config.photos.minAmount, eventConfig.photos.maxAmount))
+      .fill(``)
+      .map(() => data.photosDefaultURL + Math.random()),
     isFavorite: getRandomFlag(),
-
-    //  Каждому пункту назначения соответствует свое описание, цена, предложения, фотографии
-    //  данные сгенерырованы в destinationsData
-    description: destinationsData[randomDestination].description,
-    price: destinationsData[randomDestination].price,
-    offers: shallowCopyObjsArray(destinationsData[randomDestination].offers),
-    photos: destinationsData[randomDestination].photos
   };
 };
 
 
-export {getEventData, eventTypes, destinationTypes};
+export {getEventData, eventsData};
--- src/data/events-list.js
+++ src/data/events-list.js
@@ -1,11 +1,11 @@
 import {getRandomNumber} from '../utils/utils.js';
-import {eventsListConfig} from '../configs.js';
-import {getEventData} from './events-data.js';
+import {getEventData, eventsData} from './events-data.js';
+import {eventsListConfig, eventConfig} from '../configs.js';
 
 //  Сгенерировать список мок-данных точек
 const eventsList = new Array(getRandomNumber(eventsListConfig.minAmount, eventsListConfig.maxAmount))
   .fill(``)
-  .map(() => getEventData())
+  .map(() => getEventData(eventsData, eventConfig))
   .sort((a, b) => a.time.start - b.time.start);
 
 export {eventsList};
