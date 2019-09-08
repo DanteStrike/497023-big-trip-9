5af7a573829f78b5f5001b34a22c271ec840c465
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-09-08 22:19:09 +0300

    NEW filter controller

--- src/controllers/filters-controller.js
+++ src/controllers/filters-controller.js
@@ -1,14 +1,38 @@
-import Filters from "../components/trip-filters";
-import {render, Position} from "../utils/utils";
+import {render, Position, unrender} from '../utils/utils';
+import Filters from '../components/trip-filters';
 
 class FiltersController {
-  constructor(container) {
+  constructor(container, onFilterTypeChange) {
     this._container = container;
     this._filters = new Filters();
+    this._currentType = `everything`;
+
+    this._onFilterTypeChange = onFilterTypeChange;
+
+    this._onFiltersClick = this._onFiltersClick.bind(this);
   }
 
   init() {
+    this.show();
+    this._filters.getElement().addEventListener(`click`, this._onFiltersClick);
+  }
+
+  show() {
     render(this._container, this._filters.getElement(), Position.AFTEREND);
+  }
+
+  hide() {
+    unrender(this._filters.getElement());
+  }
+
+  _onFiltersClick(evt) {
+    if (evt.target.tagName !== `INPUT` || evt.target.value === this._currentType) {
+      return;
+    }
+
+    const newType = evt.target.value;
+    this._currentType = newType;
+    this._onFilterTypeChange(newType);
   }
 }
 
