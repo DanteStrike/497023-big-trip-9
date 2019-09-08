eee8be451bae778eebcb9f9c6497b6b333827c40
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-09-08 22:16:16 +0300

    Update events-list.js

--- src/data/events-list.js
+++ src/data/events-list.js
@@ -3,9 +3,9 @@ import {getEventData, eventsData} from './events-data.js';
 import {eventsListConfig, eventConfig} from '../configs.js';
 
 //  Сгенерировать список мок-данных точек
-const eventsListMock = new Array(getRandomNumber(eventsListConfig.minAmount, eventsListConfig.maxAmount))
+const eventsList = new Array(getRandomNumber(eventsListConfig.minAmount, eventsListConfig.maxAmount))
   .fill(``)
   .map((el, index) => getEventData(eventsData, eventConfig, index))
   .sort((a, b) => a.time.start - b.time.start);
 
-export {eventsListMock};
+export {eventsList};
