92b78d4ed4e59f9924273aa02458e5b4a2cf4ed3
Gevork Gevorkyan <bmstu.gg@gmail.com>
2019-09-13 22:27:48 +0300

    ADD defaultPointData and transferTypes

--- src/configs.js
+++ src/configs.js
@@ -1,46 +1,29 @@
 import {getAuthToken} from './utils/utils';
 
 
-export const eventConfig = {
-  descriptions: {
-    minAmount: 1,
-    maxAmount: 3
-  },
-  periodOfTime: {
-    past: -2,
-    future: 2
-  },
+export const transferTypes = new Set([`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`]);
 
-  sentences: {
-    minAmount: 1,
-    maxAmount: 3
+export const defaultPointData = {
+  'id': null,
+  'type': `flight`,
+  'destination': {
+    name: null,
+    description: null,
+    pictures: []
   },
-  photos: {
-    minAmount: 2,
-    maxAmount: 5
-  },
-  price: {
-    min: 400,
-    max: 1000
-  },
-
-  offer: {
-    price: {
-      min: 20,
-      max: 100
-    },
-    minAmount: 0,
-    maxAmount: 3
-  }
+  'date_from': Date.now(),
+  'date_to': Date.now(),
+  'base_price': 0,
+  'offers': [],
+  'is_favorite': false
 };
 
-export const eventsListConfig = {
-  minAmount: 10,
-  maxAmount: 15
+export const pointConfig = {
+  maxOffers: 3
 };
 
 export const serverConfig = {
-  endPoint: `https://htmlacademy-es-9.appspot.com/big-trip/`,
+  endPoint: `https://htmlacademy-es-9.appspot.com/big-trip`,
   authToken: `Basic ${getAuthToken()}`
 };
 
